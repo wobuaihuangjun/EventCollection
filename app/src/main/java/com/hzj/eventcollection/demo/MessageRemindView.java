faea83fba6d32c5416f53afeadf56a4ecd87df5e
huangzj <892105817@qq.com>
2017-01-06 15:06:43 +0800

    完善方案

--- app/src/main/java/com/hzj/eventcollection/demo/MessageRemindView.java
+++ app/src/main/java/com/hzj/eventcollection/demo/MessageRemindView.java
@@ -4,7 +4,6 @@ import android.content.Context;
 import android.util.AttributeSet;
 import android.view.LayoutInflater;
 import android.view.View;
-import android.widget.ImageView;
 import android.widget.RelativeLayout;
 
 import com.hzj.eventcollection.R;
@@ -16,10 +15,6 @@ import com.hzj.eventcollection.R;
  * Created by hzj on 2016/11/25.
  */
 public class MessageRemindView extends RelativeLayout implements View.OnClickListener {
-
-    private static final String TAG = "MessageRemindView";
-
-    private ImageView icon;
 
     public MessageRemindView(Context context) {
         super(context);
@@ -33,18 +28,14 @@ public class MessageRemindView extends RelativeLayout implements View.OnClickLis
 
     private void init(Context context) {
         LayoutInflater.from(context).inflate(R.layout.message_remind_view, this, true);
-        icon = (ImageView) findViewById(R.id.iv_message_remind_icon);
-        icon.setOnClickListener(this);
+        findViewById(R.id.iv_message_remind_icon).setOnClickListener(this);
     }
 
-    private void enterMessageCenter() {
-    }
 
     @Override
     public void onClick(View v) {
         switch (v.getId()) {
             case R.id.iv_message_remind_icon:
-                enterMessageCenter();
                 break;
             default:
                 break;
